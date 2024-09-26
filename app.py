53bf20b6f4ecfefb02b3c8a0ddd3c5a3d765fdf4
Benichou Mohammed El Amine <31164395+aminebenichou@users.noreply.github.com>
2024-09-26 18:22:09 +0100

    snake and food

---
+++ app.py
@@ -0,0 +1,15 @@
+class Test():
+    
+
+    def __init__(self, test_number):
+        self.test_number= test_number
+
+    def printInfo(self):
+        print(self.test_number)
+
+    class 
+
+test1 = Test(test_number=277)
+
+
+test1.printInfo()
--- snake.py
+++ snake.py
@@ -1,4 +1,5 @@
 # Example file showing a basic pygame "game loop"
+import random
 import pygame
 
 # pygame setup
@@ -7,6 +8,7 @@ screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 x, y = 100, 100
+x_food, y_food = 200, 200
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -19,11 +21,27 @@ while running:
 
     # RENDER YOUR GAME HERE
     pygame.draw.rect(screen, 'red', pygame.Rect(x, y, 20, 20))
+    # snake
+    pygame.draw.rect(screen, 'green', pygame.Rect(x_food, y_food, 20, 20))
     if pygame.key.get_pressed()[pygame.K_RIGHT]:
         x += 10
+    if pygame.key.get_pressed()[pygame.K_LEFT]:
+        x -= 10
+    if pygame.key.get_pressed()[pygame.K_UP]:
+        y -= 10
+    if pygame.key.get_pressed()[pygame.K_DOWN]:
+        y += 10
+    
+    # generate new food
+    if x_food-10 <= x and x_food+10 >=x and y_food-10 <= y and y_food+10 >=y:
+        x_food = random.randint(10,1270)
+        y_food = random.randint(10,700)
+
+
+
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(30)  # limits FPS to 60
 
 pygame.quit()
